febe4a3d8fa142d5d3a82309c2947a5e2275fc54
Dennis Murage <dmurage140@gmail.com>
2023-07-20 11:02:58 +0300

    Fixing Styling

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
+"""creates a cache"""
 
 
 import redis
 import uuid
 from typing import Union
 
+
 class Cache:
+    """_summary_
+    """
     def __init__(self):
+        """_summary_
+        """
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """_summary_
+
+        Args:
+            data (Union[str, bytes, int, float]): _description_
+
+        Returns:
+            str: _description_
+        """
         key = str(uuid.uuid4())
 
         if isinstance(data, (str, bytes, int, float)):
             self._redis.set(key, data)
 
-        # Return the generated key
         return key
-
-
